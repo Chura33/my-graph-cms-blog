1eaa006e6d142a8e8b17f95f83851ca3810439c8
Chura33 <churabrady@gmail.com>
2023-09-08 12:29:29 +0100

    added recent features

--- components/Header.jsx
+++ components/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
             <div className='md:float-left block'>
                 <Link href="/">
                     <span className='cursor-pointer font-bold text-white text-2xl'>
-                        Today's Warriors
+                        Today&apos;s Warriors
                     </span>
                 </Link>
             </div>
